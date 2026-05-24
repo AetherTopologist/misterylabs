db08c17879c0ea3d2e139cf6d6f831cdcbb9a4be
Billy <111899168+AetherTopologist@users.noreply.github.com>
2026-05-23 21:03:29 -0500

    Fix Vite React plugin + base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,16 @@ import react from '@vitejs/plugin-react';
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  plugins: [react()],
+  // This is the most important line
+  base: '/misterylabs/',
+
   server: {
     host: "::",
     port: 8080,
@@ -13,9 +21,6 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  // This is the most important line
-  base: '/misterylabs/',
-  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
